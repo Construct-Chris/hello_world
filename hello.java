431d0f29c02396869526f780c0f913930ef77dae
Construct-Chris <116314422+Construct-Chris@users.noreply.github.com>
2023-11-01 10:45:35 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -5,13 +5,9 @@ public class hello {
     public static void main(String [] args) {
 
        // initialize variables
-       String firstName = "Alex";
-
-       // delete this line
-       System.out.println("Modify this code");
+       String firstName = "Chris";
 	
        // Using the value of the variable above, output a message in the format, "Hello, I am Alex".
-
+	   System.out.println("Hello, I am " + firstName);
     }
 }
-
